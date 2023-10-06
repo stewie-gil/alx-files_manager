8bc830cec5dd604790b2e178f953fcaff3994b57
stewie-gil <gilliganstewie7@gmail.com>
2023-10-06 23:02:43 +0000

    server.js

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/AppController');
+
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+module.exports = router;
